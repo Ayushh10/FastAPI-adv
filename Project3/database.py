89a6d4a2ed5e547bbd2fef6d171de41965bf007d
Ayushh10 <aayush2sm@gmail.com>
2026-07-10 00:18:54 +0530

    Project3: SQLite3

---
+++ Project3/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False ,bind=engine)
+
+Base = declarative_base()       # object of base
