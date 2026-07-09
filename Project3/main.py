0342d7fc31c2c8349a8d5a3e4bb3ca8300af4b77
Ayushh10 <aayush2sm@gmail.com>
2026-07-10 02:01:39 +0530

    Project3: SQLite3 -- CRU Ops

--- Project3/main.py
+++ Project3/main.py
@@ -1,7 +1,51 @@
-from fastapi import FastAPI
+from typing import Annotated
+from starlette import status
+from fastapi import FastAPI, Depends, HTTPException, Path
+from sqlalchemy.orm import Session
+from todoRequest import TodoRequest
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]     #Dependency Injection
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def get_all(db : db_dependency):                  #Dependency Injection
+    return db.query(Todos).all()
+
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def get_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todos_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todos_model is not None:
+        return todos_model
+    raise HTTPException(status_code=404, detail="XXX Record not Found XXX")
+
+@app.post("/todos/create_record", status_code=status.HTTP_201_CREATED)
+async def create_record(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()  
+
+@app.put("/todos/{todo_id}", status_code=status.HTTP_201_CREATED)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail=f"Todo ID {todo_id} does not exist")
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
