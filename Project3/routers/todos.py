daaa1762e86fc909dfd35259ec4f7cdecf75498f
Ayushh10 <aayush2sm@gmail.com>
2026-07-11 22:58:59 +0530

    Project3: Routers -- Auth and Todos

--- Project3/routers/todos.py
+++ Project3/routers/todos.py
@@ -17,24 +17,24 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]     #Dependency Injection
 
-router.get("/", status_code=status.HTTP_200_OK)
+@router.get("/", status_code=status.HTTP_200_OK)
 async def get_all(db : db_dependency):                  #Dependency Injection
     return db.query(Todos).all()
 
-router.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+@router.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
 async def get_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
     todos_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todos_model is not None:
         return todos_model
     raise HTTPException(status_code=404, detail="XXX Record not Found XXX")
 
-router.post("/todos/create_record", status_code=status.HTTP_201_CREATED)
+@router.post("/todos/create_record", status_code=status.HTTP_201_CREATED)
 async def create_record(db: db_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.model_dump())
     db.add(todo_model)
     db.commit()
 
-router.put("/todos/{todo_id}", status_code=status.HTTP_201_CREATED)
+@router.put("/todos/{todo_id}", status_code=status.HTTP_201_CREATED)
 async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is None:
@@ -48,7 +48,7 @@ async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int
     db.add(todo_model)
     db.commit()
 
-router.delete("/todos/{delete_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/todos/{delete_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_todo(db : db_dependency, delete_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == delete_id).first()
     if todo_model is None: 
