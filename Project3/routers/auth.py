b12b7623f425b2eef9b9e3acd52d977787e53604
Ayushh10 <aayush2sm@gmail.com>
2026-07-15 12:38:57 +0530

    Project3: Authentication - Password Encryption

--- Project3/routers/auth.py
+++ Project3/routers/auth.py
@@ -1,25 +1,49 @@
-from fastapi import APIRouter
+from typing import Annotated
+from starlette import status
+import bcrypt
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal
 from models import Users
 from Requests import CreateUserRequest
-from routers.todos import db_dependency
 
 router = APIRouter()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+db_dependency = Annotated[Session, Depends(get_db)]     #Dependency Injection
 
-# @router.get("/auth/")
-# async def get_users():
-#     return {"User": "Authenticated"}
+@router.get("/auth/all")
+async def get_all_users(db: db_dependency):
+    return db.query(Users).all()
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+def hash_password(userPassword: str):
+    password = userPassword.encode('utf-8')
+# Generate a salt with a specific work factor (default is 12)
+    salt = bcrypt.gensalt(rounds=12)
+# Hash the password
+    hashed_password = bcrypt.hashpw(password, salt)
+    return hashed_password
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request: CreateUserRequest, db: db_dependency):
     create_new_user = Users(
         email = create_user_request.email,
         username = create_user_request.username,
         first_name = create_user_request.first_name,
         last_name = create_user_request.last_name,
         role = create_user_request.role,           
-        hashed_password = create_user_request.password,
+        hashed_password = str(hash_password(create_user_request.password)),
         is_active = True
     )
-    return create_new_user
+    db.add(create_new_user)
+    db.commit()
+    return {"New User Added": create_new_user}
+
+
+
